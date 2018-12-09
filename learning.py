5957238c521020b61662276a9cdf9bd7b505ec94
Vlad-Ștefan Harbuz <vlad@vladh.net>
2018-12-09 17:50:52 +0100

    Add nice functions

--- learning.py
+++ learning.py
@@ -27,10 +27,18 @@ def get_genders_and_last_syllables(nouninfo):
 def get_value_map(values):
     unique_values = np.unique(values)
     value_map = dict(zip(unique_values, range(0, len(unique_values))))
-    return value_map
+    value_map_inv = dict(zip(range(0, len(unique_values)), unique_values))
+
+    def do_map(d):
+        return value_map[d]
+
+    def do_map_inv(d):
+        return value_map_inv[d]
+
+    return [do_map, do_map_inv]
 
 
-def make_features_and_labels(nouninfo, gender_map, syllable_map):
+def make_features_and_labels(nouninfo, gender_to_id, syllable_to_id):
     """
     Returns [features, labels], where features are:
 
@@ -41,11 +49,11 @@ def make_features_and_labels(nouninfo, gender_map, syllable_map):
         [gender_id, ...]
     """
     features = [
-        [syllable_map[get_last_syllable(d)]]
+        [syllable_to_id(get_last_syllable(d))]
         for d in nouninfo
     ]
     labels = [
-        gender_map[get_gender(d)]
+        gender_to_id(get_gender(d))
         for d in nouninfo
     ]
     return [features, labels]
@@ -55,18 +63,19 @@ if __name__ == '__main__':
     nouninfo = data.load_nouninfo()
     genders_and_syllables = get_genders_and_last_syllables(nouninfo)
 
-    gender_map = get_value_map([d[0] for d in genders_and_syllables])
-    gender_map_inv = {v: k for k, v in gender_map.items()}
-
-    syllable_map = get_value_map([d[1] for d in genders_and_syllables])
-    syllable_map_inv = {v: k for k, v in syllable_map.items()}
+    [gender_to_id, id_to_gender] = get_value_map(
+        [d[0] for d in genders_and_syllables]
+    )
+    [syllable_to_id, id_to_syllable] = get_value_map(
+        [d[1] for d in genders_and_syllables]
+    )
 
     [features, labels] = make_features_and_labels(
-        nouninfo, gender_map, syllable_map
+        nouninfo, gender_to_id, syllable_to_id
     )
 
     clf = tree.DecisionTreeClassifier()
     clf = clf.fit(features, labels)
-    pred = clf.predict([[syllable_map['chen']]])
+    pred = clf.predict([[syllable_to_id('chen')]])
     print(pred)
-    print(gender_map_inv[pred[0]])
+    print(id_to_gender(pred[0]))
